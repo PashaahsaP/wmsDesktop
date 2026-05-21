682a2676387a89d23101708fd6aff9c848cf28d3
PashaahsaP <ppsairda@yandex.ru>
2026-05-21 19:58:03 +0700

    smt

---
+++ WmsDesktop/Pages/AddingCatalogsPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WmsDesktop.ViewModels;
+
+namespace WmsDesktop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для AddingCatalogsPage.xaml
+    /// </summary>
+    public partial class AddingCatalogsPage : Page
+    {
+       
+        public AddingCatalogViewModel viewModel;
+        public AddingCatalogsPage(MainViewModel vm, IState localVm)
+        {
+            InitializeComponent();
+            viewModel = localVm as AddingCatalogViewModel;
+        }
+         
+       
+        private void listItems_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listItems.SelectedItem != null)
+            {
+                // Получаем выбранный элемент как ListBoxItem
+                viewModel.IsEnabledNameField = false;
+                viewModel.IsEnabledAppend = false;
+                viewModel.IsEnabledSave = true;
+                var selectedItem = (OrderItem)listItems.SelectedItem;
+                tbName.Text = selectedItem.name;
+                tbSku.Text = selectedItem.sku;
+                CBSuppliersCatalog.SelectedIndex = 0;
+                viewModel.SelectedBarcodes.Clear();
+                foreach (var item in viewModel.Barcodes.Where(item => item.CatalogId == selectedItem.id))
+                {
+                    viewModel.SelectedBarcodes.Add(item);
+                };
+                for (int i = 0; i < viewModel.Suppliers.Count; i++)
+                {
+                    if(selectedItem.supplierName == viewModel.Suppliers[i].Name)
+                        CBSuppliersCatalog.SelectedIndex = i;
+                }
+            }
+            
+        }
+
+        private async void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataContext = viewModel;
+            CBSuppliers.SelectedItem = viewModel.Suppliers.First();
+        }
+    }
+}
---
+++ WmsDesktop/Pages/CreateSessionPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WmsDesktop.ViewModels;
+using WmsDesktop.vm;
+
+namespace WmsDesktop.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateSessionPage.xaml
+    /// </summary>
+    public partial class CreateSessionPage : Page
+    {
+        private readonly Client client = new Client();
+        private MainViewModel _vm = null;
+        private CreateSessionViewModel localVm = null;
+        private string ip = "192.168.0.11";
+      
+        public CreateSessionPage(MainViewModel vm, Window window, IState state)
+        {
+            InitializeComponent();
+            DataContext = state as CreateSessionViewModel;
+            _vm = vm;
+        }
+        private void WrapPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length == 1)
+                {
+                    GetDataFromFile(files[0], client);
+                }
+            }
+        }//DONE
+        private async Task GetDataFromFile(String pathToFile, Client client)
+        {
+           /* var func = AdapterHelper.getDataFromFile[localVm.Supplier];
+            var result = func(pathToFile, client, ip);
+            localVm.Items = new ObservableCollection<IUiItem>(await result);*/
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+     
+    }
+}
