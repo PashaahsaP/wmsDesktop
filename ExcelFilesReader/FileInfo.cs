30831b66715a1035b453c6d28f51cfccf237ceae
PashaahsaP <ppsairda@yandex.ru>
2026-05-26 20:14:45 +0700

    smt

--- ExcelFilesReader/FileInfo.cs
+++ ExcelFilesReader/FileInfo.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ExcelFileParser
 {
     public class FileInfo
     {
+        #region varible
+        public List<List<string>> data = new List<List<string>>();
+        #endregion
         #region prop
         public string Path {  get; set; }
         public List<string> SessionField { get; set; } = new List<string>();
         public List<Tuple<string, int>> FileField { get; set; } = new List<Tuple<string, int>>();
+        public List<List<string>> Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+                if (data.Count != 0)
+                {
+                    var firstLine = data.First();
+                    for (int i = 0; i < firstLine.Count; i++) // 0 чтобы получить чисто данные первой колонки
+                    {
+                        FileField.Add(Tuple.Create (firstLine[i], i));
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
         #endregion
         #region ctor
         public FileInfo(string path)
--- ExcelFilesReader/FileReader.cs
+++ ExcelFilesReader/FileReader.cs
@@ -15,42 +15,35 @@ namespace ExcelFileParser
             fileInfo = new FileInfo(path);
             using (var package = new ExcelPackage(fileInfo.Path))
             {
-                var art = new List<string>();
-                var barcode = new List<string>();
-                var name = new List<string>();
-                var count = new List<string>();
+                var lines = new List<List<string>>();
                 var ws = package.Workbook.Worksheets[0];
-                var temp = ws.Cells.Select(item => item.Text);
-                Console.WriteLine();
-                /*int rows = ws.Dimension.End.Row;
-                int cols = ws.Dimension.End.Column;
-                for (int r = 1; r <= rows; r++)
+
+                int lastRow = 0;
+                int lastColumn = 0;
+                if (ws.Dimension != null)
                 {
-                    for (int c = 1; c <= cols; c++)
+                    var notEmptyCells = ws.Cells.Where(cell => cell.Value != null);
+
+                    if (notEmptyCells.Any())
                     {
-                        var value = ws.Cells[r, c].Text.ToString();
-                        var valueLength = value.Length;
-                        if (c == 2 && valueLength > 0)
-                        {
-                            art.Add(value);
-                        }
-                        else if (c == 4 && valueLength > 0)
-                        {
-                            barcode.Add(value);
-                        }
-                        else if (c == 1 && valueLength > 0)
-                        {
-                            name.Add(value);
-                        }
-                        else if (c == 3 && valueLength > 0)
-                        {
-                            count.Add(value);
-                        }
-
+                        lastRow = notEmptyCells.Max(cell => cell.Start.Row);
+                        lastColumn = notEmptyCells.Max(cell => cell.Start.Column);
                     }
-                }*/
+                }
 
-            
+                for (int row = 1; row < lastRow + 1; row++)
+                {
+                    var temp = new List<string>();
+                    for (int column = 1; column < lastColumn + 1; column++)
+                    {
+                        var data = ws.Cells[row, column].Text;
+                        temp.Add(data);
+                    }
+                    lines.Add(new List<string>(temp));
+                }
+                fileInfo.Data = lines;
+                Console.WriteLine();
+
             
         }
     }
