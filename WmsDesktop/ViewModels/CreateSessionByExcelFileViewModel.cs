d09266c28fc5ba84eb4883c96c12f463458c4a25
PashaahsaP <ppsairda@yandex.ru>
2026-06-28 20:20:01 +0700

    Добавил onPropChanged и добавил selected свойство. Дальше надо сопаставить данные с основной таблицей.

--- WmsDesktop/ViewModels/CreateSessionByExcelFileViewModel.cs
+++ WmsDesktop/ViewModels/CreateSessionByExcelFileViewModel.cs
@@ -10,15 +10,69 @@ using WmsDesktop.vm;
 
 namespace WmsDesktop
 {
-    public class AddingExcelFile//view class
+    public class AddingExcelFile : INotifyPropertyChanged//view class
     {
-        public string FieldName { get; set; } = "";
-        public List<Tuple<string, int>> FileField { get; set; } = new List<Tuple<string, int>>();
+        private string _fieldName = "";
+        private List<Tuple<string, int>> _fileField = new List<Tuple<string, int>>();
+        private Tuple<string, int> _selectedItem;
+
+        public string FieldName
+        {
+            get => _fieldName;
+            set
+            {
+                if (_fieldName != value)
+                {
+                    _fieldName = value;
+                    OnPropertyChanged(nameof(FieldName)); 
+                }
+            }
+        }
+        public List<Tuple<string, int>> FileField
+        {
+            get => _fileField;
+            set
+            {
+                if (_fileField != value)
+                {
+                    _fileField = value;
+                    OnPropertyChanged(nameof(FileField)); 
+                }
+            }
+        }
+        public Tuple<string, int> SelectedItem
+        {
+            get => _selectedItem;
+            set
+            {
+                if (!EqualityComparer<Tuple<string, int>>.Default.Equals(_selectedItem, value))
+                {
+                    _selectedItem = value;
+                    OnPropertyChanged(nameof(SelectedItem)); 
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName) =>
+           PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
     
     public class CreateSessionByExcelFileViewModel : INotifyPropertyChanged
     {
-        public List<AddingExcelFile >Data { get; set; } = new List<AddingExcelFile> { };
+        private List<AddingExcelFile> _data = new List<AddingExcelFile>();
+        public List<AddingExcelFile >Data 
+        {
+            get
+            {
+                return _data;
+            }
+            set
+            {
+                _data = value;
+                OnPropertyChanged(nameof(Data));
+            }
+        } 
         public ICommand parseData { get; set; }
         public FileReader Reader { get; set; }
 
