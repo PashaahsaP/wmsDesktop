fb606451d4d5e5926b0bc4ad2a6068dd4a1f0688
Pavel <nskauramm1@gmail.com>
2026-07-15 13:23:57 +0700

    потерянное

---
+++ WmsDesktop/Pages/CreateAssemblySessionPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WmsDesktop;
+using WmsDesktop.Pages;
+using WmsDesktop.ViewModels;
+
+namespace WmsDesktop.Pages
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для CreateAssemblySessionPage.xaml
+    /// </summary>
+    public partial class CreateAssemblySessionPage : Page
+    {
+        private readonly Client client = new Client();
+        private MainViewModel _vm = null;
+        private CreateIncomeSessionViewModel localVm = null;
+        private Window _window = null;
+        public CreateAssemblySessionPage(MainViewModel vm, Window window, IState state)
+        {
+            InitializeComponent();
+            _window = window;
+            localVm = state as CreateIncomeSessionViewModel;
+            DataContext = state as CreateIncomeSessionViewModel;
+            _vm = vm;
+
+        }
+    }
+}
+
+
